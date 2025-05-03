6f88f53efb5a0316d40f21729a81193dd5ffc999
David Pinedo Srz <pinedodavsu@outlook.com>
2025-05-03 02:04:00 -0600

    Links fixed

--- src/App.tsx
+++ src/App.tsx
@@ -118,10 +118,10 @@ const App = () => {
         <p>Did you find the one piece?</p>
        <p>Versioned to Typescript and React by David Pinedo Srz.</p>
         <div className="social-links">
-          <a href="https://www.linkedin.com/in/tu-perfil-linkedin" target="_blank" rel="noopener noreferrer">
+          <a href="https://linkedin.com/in/luis-david-pinedo-suárez-8832a5298" target="_blank" rel="noopener noreferrer">
             <img src="../public/images/linkedin-logo.png" alt="LinkedIn" width="100" height="100" />
           </a>
-          <a href="https://github.com/tu-perfil-github" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/DavidPinedoSrz" target="_blank" rel="noopener noreferrer">
             <img src="../public/images/github-logo.png" alt="GitHub" width="100" height="100" />
           </a>
         </div>
